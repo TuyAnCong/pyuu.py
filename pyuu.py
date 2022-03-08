5a90337edcaf4ef4626fb67b67fbb79018a27e45
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-08 12:09:30 +0700

    Create pyuu.py

---
+++ pyuu.py
@@ -0,0 +1,14 @@
+from client, msg, bot import discord.py
+import time
+try:
+print("[LOGS] Connected with Server")
+print("Welcome to pyuu.py Console")
+def bot_Maker(): # a Bot Token in .env
+client.on(process.env.token)
+def start():
+print("[ERROR] Reloading Commands")
+bot.reloadCommand()
+file = bot.join.length("/commands/${file}.py")
+print("[LOADING] Starting now")
+print("[SUCCESS] Started")
+
